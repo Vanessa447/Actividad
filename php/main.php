5fff46a49af120b14059235ee8db08757954733f
Vanessa Agudelo <vanessa.asdi.24@gmail.com>
2024-08-16 08:21:28 -0500

    Hoy

---
+++ php/main.php
@@ -0,0 +1,26 @@
+<?php
+var= NE, NN, NMa=0 , NMe=0, P, Nota, Aux=0, Suma=0;
+
+for ($i= 1; $i < NE; $i++) {
+    for ($e= 1; $e < NN; $e++){
+        Aux== Nota
+        if(Aux > Nota){
+           NMa == Aux;
+        }elseif(Aux < Nota){
+            NMe == Nota;
+        }
+        if(Nota => 3){
+            echo "Gano";
+        } 
+        elseif(Nota < 3){
+            echo "Reprobo";
+        }
+            Suma == Suma + Nota;
+    }    
+                P == Suma / NN
+                echo "El promedio es", P;
+                echo "La nota mayor es", NMa;
+                echo "La nota menor es", NMe
+}                
+
+?>
